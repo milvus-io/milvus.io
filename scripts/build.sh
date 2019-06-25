58521a90e0f1a6ef002df748ddcc95d31bf7c77d
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2019-06-25 12:03:19 +0800

    update pdf file

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@ set -e
 MAINFONT="WenQuanYi Micro Hei"
 MONOFONT="WenQuanYi Micro Hei Mono"
 
-pandoc introduction.md --toc -V title="Milvus Documentation" -V author="Zilliz Inc." -V date="${_version_tag}" -V CJKmainfont="${MAINFONT}" -f markdown -o milvus.doc.pdf --pdf-engine=lualatex
+pandoc UserGuide.md --toc -V title="Milvus Documentation" -V author="Zilliz Inc." -V date="${_version_tag}" -V CJKmainfont="${MAINFONT}" -f markdown -o milvus.doc.pdf --pdf-engine=lualatex

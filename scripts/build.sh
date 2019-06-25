915aab8b69153b654992aad5b8365c31df2778ec
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2019-06-25 13:31:58 +0800

    generate multiple md file in docs folder

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 MAINFONT="WenQuanYi Micro Hei"
 MONOFONT="WenQuanYi Micro Hei Mono"
 
-pandoc UserGuide.md --toc -V title="Milvus Documentation" -V author="Zilliz Inc." -V date="${_version_tag}" -V CJKmainfont="${MAINFONT}" -f markdown -o milvus.doc.pdf --pdf-engine=lualatex
+for file in ./*
+do
+    if [[ $file =~ \.md$ ]]; then
+        echo $file;
+        pdfFile="$(basename ${file} | sed s:/:__:g).pdf";
+        echo "Generating pdf: $pdfFile ..."
+        pandoc $file --toc -V title="Milvus Documentation" -V author="Zilliz" -V date="${_version_tag}" -V CJKmainfont="${MAINFONT}" -f markdown -o $pdfFile --pdf-engine=lualatex;
+    fi
+done

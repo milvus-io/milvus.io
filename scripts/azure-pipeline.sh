12f4fcb4285d7e286ed7233674518967f8bd90b3
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2019-07-01 22:39:03 +0800

    update pdf

--- scripts/azure-pipeline.sh
+++ scripts/azure-pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 node merge.js userguide
 node merge.js vectordb
-node merge.js zh-CN/userguide
-node merge.js zh-CN/vectordb
+node merge.js zh-CN/userguide zh-CN
+node merge.js zh-CN/vectordb zh-CN
 docker run --rm -v `pwd`:/workspace shanghaikid/pandoc /workspace/build.sh
--- scripts/merge.js
+++ scripts/merge.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
-const [, , folder, file ='README.md'] = process.argv;
+const [, , folder, lang='en', file ='README.md' ] = process.argv;
 const tocFile = `${folder}/${file}`;
-console.log('merging tocFile', tocFile);
-
 if (!fs.existsSync(tocFile)) {
     console.log(`${tocFile} is not exist`);
 }
@@ -14,9 +12,19 @@ const linksRegexp = new RegExp(/\((.*\.md)/g);
 
 const links = markdown.match(linksRegexp).map(d => d.replace('(', ''));
 const removeFormatter = /^---(.|\n)*?---\n/;
+const yesMap = {
+    'en': 'Yes',
+    'zh-CN': '是'
+};
+const noMap = {
+    'en': 'No',
+    'zh-CN': '否'
+}
 links.forEach(link => {
     const file = fs.readFileSync(`${folder}/${link}`).toString();
-    pdf += file.replace(removeFormatter, '');
+    pdf += file.replace(removeFormatter, '')
+                    .replace(/:heavy_check_mark:/g, yesMap[lang])
+                    .replace(/:x:/g, noMap[lang]);
 });
 
 fs.writeFileSync(`./${folder}.all.md`, pdf);

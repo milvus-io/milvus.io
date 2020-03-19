5e3395c82d040e8ced7759e7fe7bc9a3b621c7d8
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2020-03-19 12:42:34 +0800

    fix refresh

--- src/templates/docTemplate.js
+++ src/templates/docTemplate.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../components/docLayout";
 import SEO from "../components/seo";
 import { graphql } from "gatsby";
 import hljs from "highlight.js";
-import LocalizeLink from '../components/localizedLink/localizedLink'
+// import LocalizeLink from "../components/localizedLink/localizedLink";
 // import sql from "highlight.js/lib/languages/sql"
 // import bash from "highlight.js/lib/languages/bash"
 import "highlight.js/styles/atom-one-dark.css";
@@ -37,20 +37,33 @@ export default function Template({
   const nav = {
     current: "doc"
   };
-  const iframeUrl = isBenchmark ? `/benchmarks/${frontmatter.id.split('_')[1]}/index.html` : ""
+  const iframeUrl = isBenchmark
+    ? `/benchmarks/${frontmatter.id.split("_")[1]}/index.html`
+    : "";
   const idRegex = /id=".*?"/g;
-  if (locale === 'cn') {
-    html = html.replace(idRegex, match => match.replace(/[？|、|，]/g, ''))
+  if (locale === "cn") {
+    html = html.replace(idRegex, match => match.replace(/[？|、|，]/g, ""));
   }
+
+  const [showBack, setShowBack] = useState(false);
 
   useEffect(() => {
     document.querySelectorAll("pre code").forEach(block => {
       hljs.highlightBlock(block);
     });
   }, []);
+
+  const ifrmLoad = () => {
+    const ifrm = document.querySelector("#inlineFrameExample");
+
+    setShowBack(!/index\.html/.test(ifrm.contentWindow.location.href));
+  };
   const handleRefresh = () => {
-    window.location.reload()
-  }
+    const ifrm = document.querySelector("#inlineFrameExample");
+    if (ifrm) {
+      ifrm.contentWindow.history.back();
+    }
+  };
   return (
     <Layout
       language={layout}
@@ -63,48 +76,48 @@ export default function Template({
       versions={versions}
       id={frontmatter.id}
       isBenchMark={isBenchmark}
-
     >
       <SEO title={`${headings[0] && headings[0].value}`} lang={locale} />
-      {
-        isBenchmark ? (
-          <div style={{ position: "relative" }}>
-            <iframe id="inlineFrameExample"
-              title="test"
-              width="100%"
-              height="2000px"
-              src={iframeUrl}></iframe>
+      {isBenchmark ? (
+        <div style={{ position: "relative" }}>
+          {showBack && (
             <i
               className="fas iframe-icon fa-arrow-left"
               onClick={handleRefresh}
             ></i>
-
+          )}
+          <iframe
+            id="inlineFrameExample"
+            title="test"
+            width="100%"
+            height="2000px"
+            src={iframeUrl}
+            onLoad={ifrmLoad}
+          ></iframe>
+        </div>
+      ) : (
+        <div className="doc-post-container">
+          <div className="doc-post">
+            <div
+              className="doc-post-content"
+              dangerouslySetInnerHTML={{ __html: html }}
+            />
           </div>
-        ) : (
-            <div className="doc-post-container">
-              <div className="doc-post">
-                <div
-                  className="doc-post-content"
-                  dangerouslySetInnerHTML={{ __html: html }}
-                />
-              </div>
-              {isBlog || isBenchmark ? null : (
-                <a
-                  className="edit-page-link button"
-                  href={`https://github.com/milvus-io/docs/tree/${version}/site/${
-                    locale === "en" ? "en" : "zh-CN"
-                    }/${editPath}`}
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  <i className="far fa-edit"></i> &nbsp; Edit
-                </a>
-              )}
-            </div>
-          )
-      }
-
-    </Layout >
+          {isBlog || isBenchmark ? null : (
+            <a
+              className="edit-page-link button"
+              href={`https://github.com/milvus-io/docs/tree/${version}/site/${
+                locale === "en" ? "en" : "zh-CN"
+              }/${editPath}`}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <i className="far fa-edit"></i> &nbsp; Edit
+            </a>
+          )}
+        </div>
+      )}
+    </Layout>
   );
 }
 
@@ -156,7 +169,7 @@ export const pageQuery = graphql`
                   txt2
                   txt3
                 }
-                tool{
+                tool {
                   title
                   txt1
                 }

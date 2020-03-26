fe8d342fefbaa1a3ff81270c371799fefd04c23e
nameczz <nameczz@nameczzdeMacBook-Pro.local>
2020-03-26 16:43:51 +0800

    add new layout

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,17 +1,11 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import { globalHistory } from "@reach/router";
 import LocalizeLink from "../localizedLink/localizedLink";
-import Logo from "../../images/logo-horizontal-white.svg";
-import Search from "../search";
+import Logo from "../../images/logo/milvus-horizontal-color.svg";
 import "./header.scss";
 
 const Header = ({ language, locale }) => {
   const { header } = language;
-  const l = locale === "cn" ? "en" : "cn";
-  const to = globalHistory.location.pathname
-    .replace("/en/", "/")
-    .replace("/cn/", "/");
 
   const [screenWidth, setScreenWidth] = useState(null);
   useEffect(() => {
@@ -34,17 +28,10 @@ const Header = ({ language, locale }) => {
         {screenWidth > 1000 ? (
           <div className="right">
             <LocalizeLink locale={locale} to="/#whymilvus" className="link">
-              {header.why}
+              {header.quick}
             </LocalizeLink>
             <LocalizeLink locale={locale} to="/gui" className="link">
               {header.gui}
-            </LocalizeLink>
-            <LocalizeLink
-              locale={locale}
-              className="link"
-              to={"/docs/guides/get_started/install_milvus/install_milvus.md"}
-            >
-              {header.tutorial}
             </LocalizeLink>
 
             <LocalizeLink locale={locale} className="link" to="/#solution">
@@ -66,32 +53,17 @@ const Header = ({ language, locale }) => {
               {header.blog}
             </LocalizeLink>
 
-            <Search language={header}></Search>
-            <a
-              href="https://github.com/milvus-io/milvus"
-              className="link"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Github
-            </a>
-            <LocalizeLink locale={l} to={to} className="link">
-              {locale === "cn" ? "EN" : "中"}
-            </LocalizeLink>
           </div>
         ) : (
             <div className="right">
-              <Search language={header}></Search>
-              <LocalizeLink locale={l} to={to}>
-                {locale === "cn" ? "EN" : "中"}
-              </LocalizeLink>
+
             </div>
           )}
       </header>
       {screenWidth <= 1000 && (
         <nav className="mobile-nav">
-          <LocalizeLink locale={locale} to="/#whymilvus">
-            {header.why}
+          <LocalizeLink locale={locale} to="/#whymilvus" className="link">
+            {header.quick}
           </LocalizeLink>
           <LocalizeLink locale={locale} to="/#solution">
             {header.solution}

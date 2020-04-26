08fbfda352bc0ef8ac152f4601ad4263a5e40d06
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2020-04-26 19:53:05 +0800

    fix maquee

--- src/components/notification/index.jsx
+++ src/components/notification/index.jsx
@@ -1,48 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./index.scss";
 
-let timer = null
-const Notification = props => {
-  const { version } = props
-  const [showSlack, setShowSlack] = useState(false)
-  useEffect(() => {
-    if (timer) return
-    timer = setInterval(() => {
-      setShowSlack(v => !v)
-    }, 15000)
-    return (() => {
-      clearInterval(timer)
-    })
-  }, [])
+// let timer = null;
+const Notification = (props) => {
+  const { version } = props;
+  // const [showSlack, setShowSlack] = useState(false);
+  // // useEffect(() => {
+  // //   if (timer) return;
+  // //   timer = setInterval(() => {
+  // //     setShowSlack((v) => !v);
+  // //   }, 3000);
+  // //   return () => {
+  // //     clearInterval(timer);
+  // //   };
+  // // }, []);
   return (
     <div className="notification">
-      <div className={`wrapper ${!showSlack && "slack-hide"}`}>
-        <span role="img" aria-label="" aria-labelledby="">
-          👋
+      <marquee text="swetha">
+        <span style={{ marginRight: "400px" }}>
+          <span role="img" aria-label="" aria-labelledby="">
+            👋
+          </span>
+          <a href={`/docs/${version}/releases/release_notes.md`}>
+            Version {version} is now available!{" "}
+            {typeof document !== "undefined" &&
+              document.body &&
+              document.body.clientWidth > 1000 &&
+              "Read about the new features and fixes."}
+          </a>
         </span>
-        <a href="https://milvusio.slack.com/join/shared_invite/zt-e0u4qu3k-bI2GDNys3ZqX1YCJ9OM~GQ"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Join the Milvus Slack channel here
-        </a>
-        {typeof document !== "undefined" &&
-          document.body &&
-          document.body.clientWidth > 1000 &&
-          "to interact with our community!"}
-      </div>
-      <div className={`wrapper version ${!showSlack && "version-show"}`}>
-        <span role="img" aria-label="" aria-labelledby="">
-          👋
+
+        <span>
+          <span role="img" aria-label="" aria-labelledby="">
+            👋
+          </span>
+          <a
+            href="https://milvusio.slack.com/join/shared_invite/zt-e0u4qu3k-bI2GDNys3ZqX1YCJ9OM~GQ"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Join the Milvus Slack channel here{" "}
+            {typeof document !== "undefined" &&
+              document.body &&
+              document.body.clientWidth > 1000 &&
+              "to interact with our community!"}
+          </a>
         </span>
-        <a href={`/docs/${version}/releases/release_notes.md`}>
-          Version {version} is now available!
-        </a>
-        {typeof document !== "undefined" &&
-          document.body &&
-          document.body.clientWidth > 1000 &&
-          "Read about the new features and fixes."}
-      </div>
+      </marquee>
     </div>
   );
 };

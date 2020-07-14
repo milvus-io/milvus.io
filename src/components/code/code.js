cd5c421d1a0e1f14f79266c36dd0cd5dd27314fc
Tumao <tumao@protonmail.com>
2020-07-14 16:44:34 +0800

    feat(code-snippet): add copy button

---
+++ src/components/code/code.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './code.scss';
+
+const Code = ({ duration, html, content }) => {
+  const [copied, setCopied] = useState(false);
+
+  const onButtonClick = async () => {
+    copyToClipboard(content);
+    setCopied(true);
+    await delay(duration);
+    setCopied(false);
+  };
+
+  const copyToClipboard = (content) => {
+    const el = document.createElement(`textarea`);
+    el.value = content;
+    el.setAttribute(`readonly`, ``);
+    el.style.position = `absolute`;
+    el.style.left = `-9999px`;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand(`copy`);
+    document.body.removeChild(el);
+  };
+
+  const delay = (duration) =>
+    new Promise((resolve) => setTimeout(resolve, duration));
+
+  return (
+    <>
+      <section className="wrapper">
+        <div dangerouslySetInnerHTML={{ __html: html }}></div>
+        <button onClick={onButtonClick} className="button-copy">
+          {copied ? `Copied` : `Copy`}
+        </button>
+      </section>
+    </>
+  );
+};
+
+Code.prototypes = {
+  duration: PropTypes.number,
+  html: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+};
+
+Code.defaultProps = {
+  duration: 3000,
+};
+
+export default Code;

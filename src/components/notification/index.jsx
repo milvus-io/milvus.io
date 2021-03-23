2912bc8707f4fc0f985633141a623a272e5b9c35
nameczz <zizhao.chen@zilliz.com>
2021-03-23 15:21:33 +0800

    fix banner url (#309)

--- src/components/notification/index.jsx
+++ src/components/notification/index.jsx
@@ -7,9 +7,9 @@ const Notification = props => {
   const { version, language, locale } = props;
   // const screenWidth = useMobileScreen();
   const notificationLinksMap = {
+    [version]: locale === 'en' ? `/docs/${version}/release_notes.md` : `/cn/docs/${version}/release_notes.md`,
     'v1.0.0': locale === 'en' ? '/docs/v1.0.0/announcement.md' : '/cn/docs/v1.0.0/announcement.md',
-    [version]: locale === 'en' ? `/docs/${version}/release_notes.md` : `/cn/docs/${version}/release_notes.md`
-  }
+  };
 
   return (
     <div className="notification">

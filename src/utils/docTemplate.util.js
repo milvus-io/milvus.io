af58e3e1f5941dbbf294d679f4677f819aa11950
ThyeeZz <41352919+ThyeeZz@users.noreply.github.com>
2021-04-16 23:40:25 +0800

    fix sort (#319)

--- src/utils/docTemplate.util.js
+++ src/utils/docTemplate.util.js
@@ -20,8 +20,11 @@ export const getAnchorElement = (selector, anchorText) => {
 export const sortVersions = (a, b) => {
   const [v1, s1, m1] = a.split('.');
   const [v2, s2, m2] = b.split('.');
-  const aValue = v1.split('')[1] * 100 + s1 * 10 + m1 * 1;
-  const bValue = v2.split('')[1] * 100 + s2 * 10 + m2 * 1;
+  
+  const aValue = v1.split('')[1] * 1000 + s1 * 10 + m1 * 1;
+  const bValue = v2.split('')[1] * 1000 + s2 * 10 + m2 * 1;
+
+  console.log(aValue,bValue)
 
   if (aValue > bValue) {
     return -1;

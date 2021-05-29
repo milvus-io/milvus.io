34974e4a74b1c1408e58306a3cea06775600e9b5
tumao <yan.wang@zilliz.com>
2021-05-29 15:41:41 +0800

    restructure gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const ReadVersionJson = require('./walkFile');
 const locales = require('./src/constants/locales');
 const DOC_LANG_FOLDERS = ['/en/', '/zh-CN/'];
-// const benchmarksMenu = require('./benchmark-menu');
 const express = require('express');
 const env = process.env.IS_PREVIEW;
 // const env = 'preview';
@@ -122,6 +121,95 @@ const findLang = path => {
   }, '');
 };
 
+const getDefaultLang = () =>
+  Object.keys(locales).find(lang => locales[lang].default);
+
+// we generate path by menu structure
+const generatePath = (
+  id,
+  lang,
+  version,
+  isBlog,
+  needLocal = true,
+  isBenchmark
+) => {
+  const defaultLang = getDefaultLang();
+  if (isBenchmark) {
+    return lang === defaultLang ? `/docs/${id}` : `${lang}/docs/${id}`;
+  }
+  if (isBlog) {
+    if (!needLocal) return `/blogs/${id}`;
+    return lang === defaultLang ? `/blogs/${id}` : `${lang}/blogs/${id}`;
+  }
+
+  let localizedPath = '';
+  if (version && version !== 'master') {
+    localizedPath =
+      lang === defaultLang ? `/docs/${version}/` : `${lang}/docs/${version}/`;
+  } else {
+    // for master branch version -> false
+    localizedPath = lang === defaultLang ? `/docs/` : `${lang}/docs/`;
+  }
+
+  return needLocal ? `${localizedPath}${id}` : `${id}`;
+};
+
+const getCommunityPath = (fileId, fileLang) => {
+  const defaultLang = getDefaultLang();
+  return fileLang === defaultLang
+    ? `/community/${fileId}`
+    : `${fileLang}/community/${fileId}`;
+};
+
+const initGlobalSearch = markdown => {
+  const flatten = arr =>
+    arr
+      .map(({ node: { frontmatter, fileAbsolutePath, headings } }) => {
+        const fileLang = findLang(fileAbsolutePath);
+
+        // const version = newestVersion || 'master'; //findVersion(fileAbsolutePath) || "master";
+        const version = findVersion(fileAbsolutePath) || 'master';
+        const headingVals = headings.map(v => v.value);
+        const isBlog = checkIsblog(fileAbsolutePath);
+        const isBenchmark = checkIsBenchmark(fileAbsolutePath);
+        const keywords = frontmatter.keywords
+          ? frontmatter.keywords.split(',')
+          : [];
+        if (keywords.length) {
+          console.log(keywords);
+        }
+        return {
+          ...frontmatter,
+          fileLang,
+          version,
+          path: generatePath(
+            frontmatter.id,
+            fileLang,
+            version,
+            isBlog,
+            false,
+            isBenchmark
+          ),
+          // the value we need compare with search query
+          values: [...headingVals, frontmatter.id, ...keywords],
+        };
+      })
+      .filter(data => data.version === newestVersion);
+
+  const fileData = flatten(markdown);
+  fs.writeFile(
+    `${__dirname}/src/search.json`,
+    JSON.stringify(fileData),
+    err => {
+      if (err) throw err;
+      console.log("It's saved!");
+    }
+  );
+};
+
+const checkIsblog = path => path.includes('blog');
+const checkIsBenchmark = path => path.includes('benchmarks');
+
 /* create pages dynamically from submodule data */
 exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions;
@@ -130,101 +218,6 @@ exports.createPages = async ({ actions, graphql }) => {
   const docTemplate = path.resolve(`src/templates/docTemplate.js`);
   const communityTemplate = path.resolve(`src/templates/community.js`);
 
-  // const markdownQueryResult = await graphql(`
-  //   {
-  //     allMarkdownRemark(
-  //       filter: { fileAbsolutePath: { regex: "/(?:site|blog)/" } }
-  //     ) {
-  //       edges {
-  //         node {
-  //           headings {
-  //             value
-  //             depth
-  //           }
-  //           frontmatter {
-  //             id
-  //             keywords
-  //           }
-  //           fileAbsolutePath
-  //           html
-  //         }
-  //       }
-  //     }
-  //   }
-  // `);
-  // if (markdownQueryResult.errors) {
-  //   console.error(markdownQueryResult.errors);
-  //   throw markdownQueryResult.errors;
-  // }
-
-  // const jsonQueryResult = await graphql(`
-  //   {
-  //     allFile(
-  //       filter: { relativeDirectory: { regex: "/(?:menuStructure|home)/" } }
-  //     ) {
-  //       edges {
-  //         node {
-  //           absolutePath
-  //           childMenuStructureJson {
-  //             menuList {
-  //               id
-  //               title
-  //               lang
-  //               label1
-  //               label2
-  //               label3
-  //               order
-  //               isMenu
-  //               outLink
-  //             }
-  //           }
-  //           childrenHomeJson {
-  //             section1 {
-  //               title
-  //               items {
-  //                 title
-  //                 key
-  //                 btnLabel
-  //                 link
-  //               }
-  //             }
-  //             section2 {
-  //               title
-  //               desc
-  //             }
-  //             section3 {
-  //               title
-  //               items {
-  //                 label
-  //                 list {
-  //                   link
-  //                   text
-  //                 }
-  //               }
-  //             }
-  //             section4 {
-  //               title
-  //               items {
-  //                 time
-  //                 title
-  //                 abstract
-  //                 imgSrc
-  //               }
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // `);
-  // if (jsonQueryResult.errors) {
-  //   console.error(jsonQueryResult.errors);
-  //   throw jsonQueryResult.errors;
-  // }
-
-  // get all menuStructures
-
-  // isMenu outLink can be add when need to use
   return graphql(`
     {
       allMarkdownRemark(
@@ -309,27 +302,6 @@ exports.createPages = async ({ actions, graphql }) => {
     if (result.errors) {
       return Promise.reject(result.errors);
     }
-    const findVersion = str => {
-      // version: v.1.0.0 | v0.x
-      const regx = /versions\/master\/([v\dx\.]*)/;
-      const match = str.match(regx);
-      return match
-        ? match[1]
-          ? match[1]
-          : env === 'preview' && str.includes('preview')
-          ? 'preview'
-          : match[1]
-        : '';
-    };
-
-    const findLang = path => {
-      return DOC_LANG_FOLDERS.reduce((pre, cur) => {
-        if (path.includes(cur)) {
-          pre = cur === '/en/' ? 'en' : 'cn';
-        }
-        return pre;
-      }, '');
-    };
 
     // get all menuStructures
     const allMenus = result.data.allFile.edges
@@ -339,10 +311,6 @@ exports.createPages = async ({ actions, graphql }) => {
         const isBlog = absolutePath.includes('blog');
         const version = findVersion(absolutePath) || 'master';
         const menuStructureList = (childMenu && [...childMenu.menuList]) || [];
-        // const benchmarkMenuList =
-        //   lang === 'en'
-        //     ? benchmarksMenu.benchmarksMenuListEN
-        //     : benchmarksMenu.benchmarksMenuListCN;
         const menuList = [...menuStructureList];
         return {
           lang,
@@ -383,92 +351,9 @@ exports.createPages = async ({ actions, graphql }) => {
       }
     );
 
-    // we generate path by menu structure
-    const generatePath = (
-      id,
-      lang,
-      version,
-      isBlog,
-      needLocal = true,
-      isBenchmark
-    ) => {
-      if (isBenchmark) {
-        return lang === defaultLang ? `/docs/${id}` : `${lang}/docs/${id}`;
-      }
-      if (isBlog) {
-        if (!needLocal) return `/blogs/${id}`;
-        return lang === defaultLang ? `/blogs/${id}` : `${lang}/blogs/${id}`;
-      }
+    // get community data: articles md, menu and home json
 
-      let localizedPath = '';
-      if (version && version !== 'master') {
-        localizedPath =
-          lang === defaultLang
-            ? `/docs/${version}/`
-            : `${lang}/docs/${version}/`;
-      } else {
-        // for master branch version -> false
-        localizedPath = lang === defaultLang ? `/docs/` : `${lang}/docs/`;
-      }
-
-      return needLocal ? `${localizedPath}${id}` : `${id}`;
-    };
-
-    const getCommunityPath = (fileId, fileLang) => {
-      return fileLang === defaultLang
-        ? `/community/${fileId}`
-        : `${fileLang}/community/${fileId}`;
-    };
-
-    const defaultLang = Object.keys(locales).find(
-      lang => locales[lang].default
-    );
-
-    // -----  for global search begin -----
-    const flatten = arr =>
-      arr
-        .map(({ node: { frontmatter, fileAbsolutePath, headings } }) => {
-          const fileLang = findLang(fileAbsolutePath);
-
-          // const version = newestVersion || 'master'; //findVersion(fileAbsolutePath) || "master";
-          const version = findVersion(fileAbsolutePath) || 'master';
-          const headingVals = headings.map(v => v.value);
-          const isBlog = checkIsblog(fileAbsolutePath);
-          const isBenchmark = checkIsBenchmark(fileAbsolutePath);
-          const keywords = frontmatter.keywords
-            ? frontmatter.keywords.split(',')
-            : [];
-          if (keywords.length) {
-            console.log(keywords);
-          }
-          return {
-            ...frontmatter,
-            fileLang,
-            version,
-            path: generatePath(
-              frontmatter.id,
-              fileLang,
-              version,
-              isBlog,
-              false,
-              isBenchmark
-            ),
-            // the value we need compare with search query
-            values: [...headingVals, frontmatter.id, ...keywords],
-          };
-        })
-        .filter(data => data.version === newestVersion);
-
-    const fileData = flatten(legalMd);
-    fs.writeFile(
-      `${__dirname}/src/search.json`,
-      JSON.stringify(fileData),
-      err => {
-        if (err) throw err;
-        console.log("It's saved!");
-      }
-    );
-    // -----  for global search end -----
+    initGlobalSearch(legalMd);
 
     // create doc home page
     homeData.forEach(({ language, data, path }) => {
@@ -494,39 +379,32 @@ exports.createPages = async ({ actions, graphql }) => {
       });
     });
 
-    console.log('====== after doc home create page');
-
-    return legalMd.forEach(({ node }) => {
+    // create Milvus docs
+    legalMd.forEach(({ node }) => {
       const fileAbsolutePath = node.fileAbsolutePath;
       const fileId = node.frontmatter.id;
       let version = findVersion(fileAbsolutePath) || 'master';
 
       const fileLang = findLang(fileAbsolutePath);
-      const isCommunityPage = fileAbsolutePath.includes('communityArticles');
-
       const editPath = fileAbsolutePath.split(
         fileLang === 'en' ? '/en/' : '/zh-CN/'
       )[1];
       const isBlog = checkIsblog(fileAbsolutePath);
       const isBenchmark = checkIsBenchmark(fileAbsolutePath);
-      const localizedPath = !isCommunityPage
-        ? generatePath(fileId, fileLang, version, isBlog, true, isBenchmark)
-        : getCommunityPath(fileId, fileLang);
+      const localizedPath = generatePath(
+        fileId,
+        fileLang,
+        version,
+        isBlog,
+        true,
+        isBenchmark
+      );
 
       const newHtml = node.html;
 
-      // if (isCommunityPage) {
-      //   console.log(
-      //     '===== community page info',
-      //     localizedPath,
-      //     `language: ${fileLang}\n`,
-      //     newHtml
-      //   );
-      // }
-
       // the newest doc version is master so we need to make route without version.
       // for easy link to the newest doc
-      if (version === newestVersion && !isCommunityPage) {
+      if (version === newestVersion) {
         const masterPath = isBenchmark
           ? `/docs/$${fileId}`
           : generatePath(fileId, fileLang, isBlog ? false : 'master', isBlog);
@@ -551,41 +429,27 @@ exports.createPages = async ({ actions, graphql }) => {
         });
       }
 
-      //  normal pages
-      return !isCommunityPage
-        ? createPage({
-            path: localizedPath,
-            component: docTemplate,
-            context: {
-              locale: fileLang,
-              version: isBenchmark ? newestVersion : version,
-              versions: Array.from(versions),
-              old: fileId,
-              headings: node.headings.filter(v => v.depth < 4 && v.depth >= 1),
-              fileAbsolutePath,
-              localizedPath,
-              newestVersion,
-              isBlog,
-              editPath,
-              allMenus,
-              isBenchmark,
-              newHtml,
-              homeData: null,
-            }, // additional data can be passed via context
-          })
-        : null;
-      // createPage({
-      //     path: localizedPath,
-      //     component: communityTemplate,
-      //     context: {
-      //       // locale: fileLang,
-      //       fileAbsolutePath: path,
-      //       // newHtml,
-      //     },
-      //   });
+      // normal pages
+      createPage({
+        path: localizedPath,
+        component: docTemplate,
+        context: {
+          locale: fileLang,
+          version: isBenchmark ? newestVersion : version,
+          versions: Array.from(versions),
+          old: fileId,
+          headings: node.headings.filter(v => v.depth < 4 && v.depth >= 1),
+          fileAbsolutePath,
+          localizedPath,
+          newestVersion,
+          isBlog,
+          editPath,
+          allMenus,
+          isBenchmark,
+          newHtml,
+          homeData: null,
+        }, // additional data can be passed via context
+      });
     });
   });
 };
-
-const checkIsblog = path => path.includes('blog');
-const checkIsBenchmark = path => path.includes('benchmarks');

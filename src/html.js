8e82edcf36fb9b78af5264b64394e0255dc73fcb
ThyeeZz <jialian.ji@zilliz.com>
2021-05-31 18:21:16 +0800

    remove font awesome link

--- src/html.js
+++ src/html.js
@@ -17,10 +17,6 @@ export default function HTML(props) {
         />
         <link
           rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
-        />
-        <link
-          rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css"
         />
         <link

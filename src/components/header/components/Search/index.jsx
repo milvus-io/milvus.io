2b8b35bae01b5b76b8b163174c9cf2a0c2ce0c74
tumao <yan.wang@zilliz.com>
2021-06-03 14:34:15 +0800

    reset algolia search style in css

--- src/components/header/components/Search/index.jsx
+++ src/components/header/components/Search/index.jsx
@@ -1,19 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import * as searchStyles from './index.module.less';
-
 const Search = () => {
-  useEffect(() => {
-    const algoliaInputWrapper = document.querySelector('.algolia-autocomplete');
-    const algoliaMenu = document.querySelector('.ds-dropdown-menu');
-    if (algoliaInputWrapper !== null) {
-      algoliaInputWrapper.style.width = '100%';
-    }
-    if (algoliaMenu !== null) {
-      algoliaMenu.style.minWidth = '100%';
-      algoliaMenu.style.top = '160%';
-    }
-  });
-
   return (
     <div className={searchStyles.searchSection}>
       <div className={searchStyles.inputWrapper}>

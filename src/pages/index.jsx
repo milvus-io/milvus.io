cece6fad0789442d6622e9b9882e319031a603f7
ThyeeZz <jialian.ji@zilliz.com>
2021-06-18 19:07:52 +0800

    fix error

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,45 @@ import Button from '../components/button';
 import GithubButton from 'react-github-button';
 import './index.less';
 import Seo from '../components/seo';
-import icons from './assets';
+import iqiyi from '../images/v2/iqiyi.svg';
+import vipcom from '../images/v2/vipcom.svg';
+import wps from '../images/v2/wps.svg';
+import trend from '../images/v2/trend.svg';
+import zs from '../images/v2/zs.svg';
+import axa from '../images/v2/axa.svg';
+import xiaomi from '../images/v2/xiaomi.svg';
+import feishu from '../images/v2/feishu.svg';
+import arcelor from '../images/v2/arcelor.svg';
+import walmart from '../images/v2/walmart.svg';
+import dxy from '../images/v2/dxy.svg';
+import alibaba from '../images/v2/alibaba.svg';
+import autoscalingIcon from '../images/v2/autoscaling.svg';
+import deploymentIcon from '../images/v2/deployment.svg';
+import searchIcon from '../images/v2/searchicon.svg';
+import storageIcon from '../images/v2/storage.svg';
+import supportIcon from '../images/v2/support.svg';
+import bgImg3 from '../images/v2/bgImg3.svg';
+
+const icons = {
+  autoscaling: autoscalingIcon,
+  deployment: deploymentIcon,
+  search: searchIcon,
+  storage: storageIcon,
+  supporting: supportIcon,
+  bgImg3,
+  iqiyi,
+  vipcom,
+  wps,
+  trend,
+  zs,
+  axa,
+  xiaomi,
+  feishu,
+  arcelor,
+  walmart,
+  dxy,
+  alibaba
+};
 
 const HomePage = ({ data, pageContext }) => {
   const {

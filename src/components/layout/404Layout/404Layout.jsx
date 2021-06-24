c3d09ab8dbf65a539c8e456cabd7bf535fcb4068
ThyeeZz <jialian.ji@zilliz.com>
2021-06-24 11:42:23 +0800

    delete useless comment

--- src/components/layout/404Layout/404Layout.jsx
+++ src/components/layout/404Layout/404Layout.jsx
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../../header/v2';

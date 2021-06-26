024572588971c74873d74cc4a49c213ea9ac1688
tumao <yan.wang@zilliz.com>
2021-06-26 10:59:49 +0800

    format code

--- src/utils/docTemplate.util.js
+++ src/utils/docTemplate.util.js
@@ -13,17 +13,17 @@ export const scrollToElement = (element, offset = 62) => {
 
 export const getAnchorElement = (selector, anchorText) => {
   return Array.from(document.querySelectorAll(selector)).find(
-    (e) => e.textContent === anchorText
+    e => e.textContent === anchorText
   );
 };
 
 export const sortVersions = (a, b) => {
   if (a && b) {
-    const [v1, s1, m1] = a.split(".");
-    const [v2, s2, m2] = b.split(".");
+    const [v1, s1, m1] = a.split('.');
+    const [v2, s2, m2] = b.split('.');
 
-    let aValue = v1.split("")[1] * 10000 + s1 * 100 + m1 * 1;
-    let bValue = v2.split("")[1] * 10000 + s2 * 100 + m2 * 1;
+    let aValue = v1.split('')[1] * 10000 + s1 * 100 + m1 * 1;
+    let bValue = v2.split('')[1] * 10000 + s2 * 100 + m2 * 1;
 
     aValue = aValue ? aValue : 0;
     bValue = bValue ? bValue : 0;
@@ -41,46 +41,46 @@ export const sortVersions = (a, b) => {
 };
 
 export const importAllPics = (r, type, users = [], resources = []) => {
-  r.keys().forEach((key) => {
+  r.keys().forEach(key => {
     const m = r(key);
     const matchs = key.match(/.\/(\S*).svg/);
-    let href = "";
+    let href = '';
     let order = 0;
-    if (type === "resources" && matchs.length) {
+    if (type === 'resources' && matchs.length) {
       switch (matchs[1]) {
-        case "bilibili":
+        case 'bilibili':
           order = 4;
           href =
-            "https://space.bilibili.com/478166626?from=search&seid=1306120686699362786";
+            'https://space.bilibili.com/478166626?from=search&seid=1306120686699362786';
           break;
-        case "medium":
+        case 'medium':
           order = 2;
-          href = "https://medium.com/unstructured-data-service";
+          href = 'https://medium.com/unstructured-data-service';
           break;
-        case "slack":
+        case 'slack':
           order = 0;
           href =
-            "https://milvusio.slack.com/join/shared_invite/zt-e0u4qu3k-bI2GDNys3ZqX1YCJ9OM~GQ";
+            'https://milvusio.slack.com/join/shared_invite/zt-e0u4qu3k-bI2GDNys3ZqX1YCJ9OM~GQ';
           break;
-        case "twitter":
+        case 'twitter':
           order = 1;
-          href = "https://twitter.com/milvusio";
+          href = 'https://twitter.com/milvusio';
           break;
-        case "zhihu":
+        case 'zhihu':
           order = 5;
-          href = "https://zhuanlan.zhihu.com/ai-search";
+          href = 'https://zhuanlan.zhihu.com/ai-search';
           break;
-        case "wechat":
+        case 'wechat':
           order = 3;
-          href = "#";
+          href = '#';
           break;
         default:
-          href = "#";
+          href = '#';
           break;
       }
     }
-    if (type === "users") {
-      const index = key.replace(/[^0-9]/gi, "");
+    if (type === 'users') {
+      const index = key.replace(/[^0-9]/gi, '');
       users[index] = m;
     } else {
       resources[order] = { src: m, name: matchs && matchs[1], href };
@@ -88,10 +88,10 @@ export const importAllPics = (r, type, users = [], resources = []) => {
   });
 };
 
-export const getStyleType = (version) => {
-  return version.startsWith("v1") ? "new" : "old";
+export const getStyleType = version => {
+  return version.startsWith('v1') ? 'new' : 'old';
 };
 
-export const getHeaderHeight = (type) => {
-  return type === "new" ? "90px" : "60px";
+export const getHeaderHeight = type => {
+  return type === 'new' ? '90px' : '60px';
 };

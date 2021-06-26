2acc3853783e04b9194f7faf56253d0c0dccb763
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2021-06-26 21:20:58 +0800

    milvus -> Milvus

--- src/components/header/v2/constants.js
+++ src/components/header/v2/constants.js
@@ -1,59 +1,59 @@
 export const LANGUAGES = [
   {
-    label: "中文",
-    value: "cn",
+    label: '中文',
+    value: 'cn',
   },
   {
-    label: "English",
-    value: "en",
+    label: 'English',
+    value: 'en',
   },
 ];
 
 export const NAVLIST_EN = [
   {
-    label: "What is milvus?",
-    link: "/docs/overview.md",
+    label: 'What is Milvus?',
+    link: '/docs/overview.md',
     isExternal: false,
   },
   {
-    label: "Documentation",
-    link: "/docs/home",
-    keyWord: "/docs",
+    label: 'Documentation',
+    link: '/docs/home',
+    keyWord: '/docs',
     isExternal: false,
   },
   {
-    label: "Blog",
-    link: "https://blog.milvus.io/",
+    label: 'Blog',
+    link: 'https://blog.milvus.io/',
     isExternal: true,
   },
   {
-    label: "Community",
-    link: "/community",
-    keyWord: "/community",
+    label: 'Community',
+    link: '/community',
+    keyWord: '/community',
     isExternal: false,
   },
 ];
 export const NAVLIST_CN = [
   {
-    label: "什么是 milvus?",
-    link: "/docs/overview.md",
+    label: '什么是 milvus?',
+    link: '/docs/overview.md',
     isExternal: false,
   },
   {
-    label: "文档",
-    link: "/docs/home",
-    keyWord: "/docs",
+    label: '文档',
+    link: '/docs/home',
+    keyWord: '/docs',
     isExternal: false,
   },
   {
-    label: "博客",
-    link: "https://blog.milvus.io/",
+    label: '博客',
+    link: 'https://blog.milvus.io/',
     isExternal: true,
   },
   {
-    label: "社区",
-    link: "/community",
-    keyWord: "/community",
+    label: '社区',
+    link: '/community',
+    keyWord: '/community',
     isExternal: false,
   },
 ];

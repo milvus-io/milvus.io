669a5a638d8d87ed341b644f6b2e404050bd0172
ThyeeZz <jialian.ji@zilliz.com>
2021-06-28 15:50:04 +0800

    resolve build error

--- src/components/card/solutionCard/index.jsx
+++ src/components/card/solutionCard/index.jsx
@@ -10,24 +10,39 @@ const SolutionCard = ({
   liveDemo = '',
 }) => {
   return (
-    <a
-      className={`${styles.solutionCard} ${className}`}
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <div className={styles.titleBar}>
-        <h3 className={styles.title}>{title}</h3>
-        {img ? <img src={img} alt="icon" className={styles.img} /> : null}
-      </div>
-
-      <p className={styles.content}>{content}</p>
+    <>
       {liveDemo ? (
-        <a href={liveDemo} target="_blank" rel="noopener noreferrer">
-          Live Demo
-        </a>
-      ) : null}
-    </a>
+        <>
+          <div className={`${styles.solutionCard} ${className}`}>
+            <div className={styles.titleBar}>
+              <h3 className={styles.title}>{title}</h3>
+              {img ? <img src={img} alt="icon" className={styles.img} /> : null}
+            </div>
+
+            <p className={styles.content}>{content}</p>
+            <a href={liveDemo} target="_blank" rel="noopener noreferrer">
+              Live Demo
+            </a>
+          </div>
+        </>
+      ) : (
+        <>
+          <a
+            className={`${styles.solutionCard} ${className}`}
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className={styles.titleBar}>
+              <h3 className={styles.title}>{title}</h3>
+              {img ? <img src={img} alt="icon" className={styles.img} /> : null}
+            </div>
+
+            <p className={styles.content}>{content}</p>
+          </a>
+        </>
+      )}
+    </>
   );
 };
 

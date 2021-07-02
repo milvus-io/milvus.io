7cb5c522e0fdc28c67871ca67f47fa67fea9510b
Tumao <yan.wang@zilliz.com>
2021-07-02 18:39:45 +0800

    Preview (#539)
    * modify styles
    
    * fix sub title style
    
    * issue#506
    
    * modify styles (#532)
    
    * modify styles
    
    * fix sub title style
    
    * issue#506
    
    * fix ci
    
    * fix the link of blog in doc home
    
    * remove sonsole
    
    * update submodules (#540)
    
    Co-authored-by: ThyeeZz <jialian.ji@zilliz.com>
    Co-authored-by: ThyeeZz <41352919+ThyeeZz@users.noreply.github.com>
    Co-authored-by: nameczz <zizhao.chen@zilliz.com>
    Co-authored-by: nameczz <nameczz@126.com>

--- src/components/card/blogCard/blogCard.jsx
+++ src/components/card/blogCard/blogCard.jsx
@@ -3,18 +3,23 @@ import * as styles from './blogCard.module.less';
 import { useMobileScreen } from '../../../hooks/index';
 
 const BlogCard = ({ data, wrapperClass = '' }) => {
-  const { title, abstract, imgSrc, time } = data;
+  const { title, abstract, imgSrc, time, link } = data;
   const { isMobile } = useMobileScreen();
 
   return (
-    <div className={`${styles.blogWrapper} ${wrapperClass}`}>
+    <a
+      className={`${styles.blogWrapper} ${wrapperClass}`}
+      href={link}
+      target="_blank"
+      rel="noreferrer"
+    >
       <div className={styles.content}>
         <p className={styles.time}>{time}</p>
         <h2>{title}</h2>
         <p className={styles.abstract}>{abstract}</p>
       </div>
       {!isMobile && <img src={imgSrc} alt="blog" />}
-    </div>
+    </a>
   );
 };
 

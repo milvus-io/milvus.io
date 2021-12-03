e31256a85013dd86dee38e43b889923c36018fbf
ThyeeZz <41352919+ThyeeZz@users.noreply.github.com>
2021-12-03 18:49:01 +0800

    Preview (#778)
    * modify font family of doc search modal (#770)
    
    * modify font family of doc search modal
    
    * modify font family
    
    * add links in demo page
    
    * add options to multiple code block
    
    * update youtube link
    
    * complete
    
    * optimized
    
    * update style
    
    * optimized

--- src/components/swiper/index.jsx
+++ src/components/swiper/index.jsx
@@ -1,24 +1,56 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as styles from './index.module.less';
 import { useMobileScreen } from '../../hooks';
 
+const getArrItem = (list, index) => {
+  if (index < 0) {
+    return getArrItem(list, index + list.length);
+  }
+  return list[index];
+};
+
 const Swiper = ({ list, duration = 10000 }) => {
-  const animateContainer = useRef(null);
   const wrapper = useRef(null);
   const { screenWidth } = useMobileScreen();
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  // need to hide the item that is moved from the far left to the far right,
+  // because it will overlap when it moves through the window.
+  const getHiddenItem = (activeIndex, currentIndex) => {
+    let temp =
+      activeIndex - 2 < 0 ? activeIndex - 2 + list.length : activeIndex - 2;
+    if (temp === currentIndex) {
+      return true;
+    }
+    return false;
+  };
 
   useEffect(() => {
-    const len = list.length - 1;
-    const width = wrapper.current.clientWidth;
-    let [timer, index] = [null, 0];
+    const [len, width] = [list.length - 1, wrapper.current.clientWidth];
+    let [timer, nextIndex] = [null, 0];
+    const swiperItemList = Array.from(
+      document.querySelectorAll('.swiper-item')
+    );
+
+    // generate circular track
+    const loopArr = Array.from({ length: list.length }).map((_, i) => {
+      if (i === list.length - 1) {
+        return -1 * width;
+      }
+      return i * width;
+    });
+
     if (timer) {
       clearInterval(timer);
     }
+
     timer = setInterval(() => {
-      index = index < len ? index + 1 : 0;
-      animateContainer.current.style.transform = `translateX(${-(
-        index * width
-      )}px)`;
+      nextIndex = nextIndex < len ? nextIndex + 1 : 0;
+      setActiveIndex(nextIndex);
+
+      swiperItemList.forEach((item, idx) => {
+        item.style.left = `${getArrItem(loopArr, idx - nextIndex)}px`;
+      });
     }, duration);
 
     return () => {
@@ -26,18 +58,22 @@ const Swiper = ({ list, duration = 10000 }) => {
         clearInterval(timer);
       }
     };
-  }, [screenWidth]);
+  }, [screenWidth, duration, list.length]);
 
   return (
     <div className={styles.swiperContainer} ref={wrapper}>
-      <ul className={styles.listWrapper} ref={animateContainer}>
-        {list.map(item => (
-          <li className={styles.listItem} key={item.text}>
-            <p className={styles.content}>{item.text}</p>
-            <p className={styles.author}>{item.author}</p>
-          </li>
-        ))}
-      </ul>
+      {list.map((item, index) => (
+        <div
+          className={`${styles.swiperItem} ${
+            getHiddenItem(activeIndex, index) ? styles.hidden : ''
+          } swiper-item`}
+          key={item.text}
+          style={{ left: `${100 * index}%` }}
+        >
+          <p className={styles.content}>{item.text}</p>
+          <p className={styles.author}>{item.author}</p>
+        </div>
+      ))}
     </div>
   );
 };

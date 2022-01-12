a19590be0f959a291532fdc9f6d1a589e6ab998d
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2022-01-12 20:24:08 +0800

    update title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 let gatsbyConfigs = {
   siteMetadata: {
     siteUrl: `https://milvus.io`,
-    title: `· Open Source Vector Database designed for AI applications`,
+    title: `· Open Source Vector Database built for scalable similarity search`,
     description: `Milvus is the world's most advanced open-source vector database, built for developing and maintaining AI applications. Powered by Faiss, NMSLIB, and Annoy, the platform supports real-time vector similarity search on massive, trillion-scale datasets.`,
     author: `@ZILLIZ.com`,
   },

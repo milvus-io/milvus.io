725be1322345247747ccf94305935ea07e5b59a7
ThyeeZz <jialian.ji@zilliz.com>
2022-02-09 19:33:41 +0800

    fix sizing tool url

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -279,7 +279,7 @@ const Header = ({ darkMode = false, t = v => v, className = "" }) => {
                 </a>
               </MenuItem>
               <MenuItem onClick={handleMenuLinkClick}>
-                <Link to="/sizing-tool" className={styles.menuLink}>
+                <Link to="/tools/sizing" className={styles.menuLink}>
                   Sizing Tool
                 </Link>
               </MenuItem>
@@ -395,7 +395,7 @@ const Header = ({ darkMode = false, t = v => v, className = "" }) => {
                       >
                         Milvus_CLI
                       </a>
-                      <Link to="/sizing-tool" className={styles.mobileMenuLink}>
+                      <Link to="/tools/sizing" className={styles.mobileMenuLink}>
                         Sizing Tool
                       </Link>
                     </>

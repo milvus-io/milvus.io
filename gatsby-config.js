2f4345e86045d98239a8ca453b8bcf844ff45135
iynewz <81401074+iynewz@users.noreply.github.com>
2022-02-27 17:49:53 +0800

    add react cookie consent component

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,19 +20,19 @@ let gatsbyConfigs = {
           cookieName: 'gatsby-gdpr-google-tagmanager', // default
           dataLayerName: 'dataLayer', // default
         },
-        facebookPixel: {
-          pixelId: 'YOUR_FACEBOOK_PIXEL_ID', // leave empty if you want to disable the tracker
-          cookieName: 'gatsby-gdpr-facebook-pixel', // default
-        },
-        tikTokPixel: {
-          pixelId: 'YOUR_TIKTOK_PIXEL_ID', // leave empty if you want to disable the tracker
-          cookieName: 'gatsby-gdpr-tiktok-pixel', // default
-        },
-        hotjar: {
-          hjid: 'YOUR_HOTJAR_ID',
-          hjsv: 'YOUR_HOTJAR_SNIPPET_VERSION',
-          cookieName: 'gatsby-gdpr-hotjar', // default
-        },
+        // facebookPixel: {
+        //   pixelId: 'YOUR_FACEBOOK_PIXEL_ID', // leave empty if you want to disable the tracker
+        //   cookieName: 'gatsby-gdpr-facebook-pixel', // default
+        // },
+        // tikTokPixel: {
+        //   pixelId: 'YOUR_TIKTOK_PIXEL_ID', // leave empty if you want to disable the tracker
+        //   cookieName: 'gatsby-gdpr-tiktok-pixel', // default
+        // },
+        // hotjar: {
+        //   hjid: 'YOUR_HOTJAR_ID',
+        //   hjsv: 'YOUR_HOTJAR_SNIPPET_VERSION',
+        //   cookieName: 'gatsby-gdpr-hotjar', // default
+        // },
         // defines the environments where the tracking should be available  - default is ["production"]
         environments: ['production', 'development']
       },

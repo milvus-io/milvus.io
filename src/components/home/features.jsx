807c72970fd271baeeae7b158cbf53375dafea7e
ThyeeZz <jialian.ji@zilliz.com>
2022-08-23 18:58:10 +0800

    update features

--- src/components/home/features.jsx
+++ src/components/home/features.jsx
@@ -3,8 +3,8 @@ import CloudIcon from '../../images/home/cloud.svg';
 import RichIcon from '../../images/home/rich.svg';
 import EasyIcon from '../../images/home/easy.svg';
 import ScaleIcon from '../../images/home/scale.svg';
-import CostIcon from '../../images/home/cost.svg';
 import FastIcon from '../../images/home/fast.svg';
+import AvailableIcon from '../../images/home/available.svg';
 
 const HomeBanner = props => {
   const { t = v => v } = props;
@@ -15,33 +15,36 @@ const HomeBanner = props => {
       <h5>{t('v3trans.home.feature.desc')}</h5>
       <ul className="features">
         <li>
-          <img src={EasyIcon} alt={t('v3trans.home.feature.easy')} />
+          <img src={EasyIcon} alt={t('v3trans.home.feature.easy.title')} />
           <h4>{t('v3trans.home.feature.easy.title')}</h4>
           <p>{t('v3trans.home.feature.easy.desc')}</p>
         </li>
         <li>
-          <img src={FastIcon} alt={t('v3trans.home.feature.fast')} />
+          <img src={FastIcon} alt={t('v3trans.home.feature.fast.title')} />
           <h4>{t('v3trans.home.feature.fast.title')}</h4>
           <p>{t('v3trans.home.feature.fast.desc')}</p>
         </li>
         <li>
-          <img src={ScaleIcon} alt={t('v3trans.home.feature.scale')} />
+          <img
+            src={AvailableIcon}
+            alt={t('v3trans.home.feature.available.title')}
+          />
           <h4>{t('v3trans.home.feature.available.title')}</h4>
           <p>{t('v3trans.home.feature.available.desc')}</p>
         </li>
         <li>
-          <img src={CostIcon} alt={t('v3trans.home.feature.cost')} />
+          <img src={ScaleIcon} alt={t('v3trans.home.feature.scale.title')} />
           <h4>{t('v3trans.home.feature.scalable.title')}</h4>
           <p>{t('v3trans.home.feature.scalable.desc')}</p>
         </li>
         <li>
-          <img src={CloudIcon} alt={t('v3trans.home.feature.cloud')} />
+          <img src={CloudIcon} alt={t('v3trans.home.feature.cloud.title')} />
           <h4>{t('v3trans.home.feature.cloud.title')}</h4>
           <p>{t('v3trans.home.feature.cloud.desc')}</p>
         </li>
 
         <li>
-          <img src={RichIcon} alt={t('v3trans.home.feature.rich')} />
+          <img src={RichIcon} alt={t('v3trans.home.feature.rich.title')} />
           <h4>{t('v3trans.home.feature.rich.title')}</h4>
           <p>{t('v3trans.home.feature.rich.desc')}</p>
         </li>

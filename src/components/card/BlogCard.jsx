da599d15cdf25cc268f9eea1efc486e762e404cf
ThyeeZz <jialian.ji@zilliz.com>
2023-08-30 15:25:24 +0800

    update brand wall
    Signed-off-by: ThyeeZz <jialian.ji@zilliz.com>

--- src/components/card/BlogCard.jsx
+++ src/components/card/BlogCard.jsx
@@ -16,7 +16,7 @@ const BlogCard = ({ title, desc, tags, cover, locale, path, className }) => {
           <Tags list={tags} tagsClass={styles.tags} />
         </div>
         <h6 className={styles.title}>{title}</h6>
-        <p className={styles.desc}>{desc}</p>
+        {/* <p className={styles.desc}>{desc}</p> */}
       </div>
     </Link>
   );

f587864ef86bd9571a8fbf49fa154d3221f57581
ThyeeZz <jialian.ji@zilliz.com>
2024-06-04 11:33:57 +0800

    prevent prefetch on anchor

--- src/parts/docs/anchorTree/index.jsx
+++ src/parts/docs/anchorTree/index.jsx
@@ -38,9 +38,7 @@ export default function AnchorTree(props) {
               key={v.label + idx}
               onClick={() => handleChooseAnchor(href)}
             >
-              <Link href={`#${href}`} prefetch={false}>
-                {label}
-              </Link>
+              <a href={`#${href}`}>{label}</a>
             </li>
           );
         })}

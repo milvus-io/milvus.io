f4ab1fa00497e7375ee7152a1a19670dbbde98d5
ThyeeZz <jialian.ji@zilliz.com>
2024-06-04 09:51:04 +0800

    update commit info bar

--- src/parts/docs/GitCommitInfo.jsx
+++ src/parts/docs/GitCommitInfo.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import classes from './gitCommit.module.less';
 
 export default function GitCommitInfo(props) {
   const { commitInfo = {}, mdId, commitTrans = 'was last updated at' } = props;
   return (
-    <div className="commit-info-wrapper">
+    <div className={classes.commitInfoWrapper}>
       <a target="_blank" rel="noreferrer" href={commitInfo.source}>
         {mdId}
       </a>

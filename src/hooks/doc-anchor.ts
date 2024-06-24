b9121f0b794b9db9582eb124d2bf0890a085cf40
ThyeeZz <jialian.ji@zilliz.com>
2024-06-24 12:42:58 +0800

    upgrade doc anchors

---
+++ src/hooks/doc-anchor.ts
@@ -0,0 +1,47 @@
+import { useEffect, useRef, useState } from 'react';
+import { DocAnchorItemType } from '@/types/docs';
+import { useRouter } from 'next/router';
+
+export const useActivateAnchorWhenScroll = ({
+  articleContainer,
+  anchorList,
+}: {
+  articleContainer: React.MutableRefObject<HTMLDivElement | null>;
+  anchorList: DocAnchorItemType[];
+}) => {
+  const [activeAnchor, setActiveAnchor] = useState('');
+
+  useEffect(() => {
+    const container = articleContainer.current;
+    if (typeof window === 'undefined' || !window || !container) {
+      return;
+    }
+    const anchorEle = 'h2';
+    const defaultDistance = 130; // 60 + header：70px
+
+    const h2List = Array.from(container.querySelectorAll(anchorEle));
+    const h1Ele = container.querySelector('h1');
+    const anchors = [h1Ele, ...h2List];
+
+    const handleScroll = () => {
+      // if the distance from top of current h2 is less than 60px , we activate this h2
+      for (let anchor of anchors) {
+        const id = anchor.getAttribute('id');
+        const top = anchor.getClientRects()[0]?.top;
+        if (top < defaultDistance && top > 0) {
+          const href = anchorList.find(v => v.href === id)?.href || '';
+          setActiveAnchor(href);
+          // in case two h2 are too close to misjudged
+          break;
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [articleContainer, anchorList]);
+
+  return activeAnchor;
+};
--- src/parts/docs/anchorTree/index.tsx
+++ src/parts/docs/anchorTree/index.tsx
@@ -3,29 +3,28 @@ import Link from 'next/link';
 import clsx from 'clsx';
 import React, { useState } from 'react';
 import { TOCIcon } from '@/components/icons';
+import { useTranslation } from 'react-i18next';
+import { DocAnchorItemType } from '@/types/docs';
 
-export default function AnchorTree(props) {
-  const { list, t, className: customClassName = '' } = props;
+export default function AnchorTree(props: {
+  list: DocAnchorItemType[];
+  activeAnchor: string;
+  className?: string;
+}) {
+  const { t } = useTranslation('docs');
+  const { list, activeAnchor, className: customClassName = '' } = props;
 
   // label maybe duplicate, we use href as unique key to show active style
-  const [activeAnchor, setActiveAnchor] = useState('');
 
   const headingLevelList = Array.from(
     new Set(list.map(item => item.type))
   ).sort();
 
-  const handleChooseAnchor = href => {
-    if (!href) {
-      return;
-    }
-    setActiveAnchor(href);
-  };
-
   return (
     <div className={classes.anchorsWrapper}>
       <h4 className={classes.tocWrapper}>
         <TOCIcon />
-        <span className={classes.tocTitle}>Table of contents</span>
+        <span className={classes.tocTitle}>{t('anchors.title')}</span>
       </h4>
 
       <ul className={clsx(classes.anchorsList, customClassName)}>
@@ -40,7 +39,6 @@ export default function AnchorTree(props) {
                 [classes.activeAnchorItem]: href === activeAnchor,
               })}
               key={v.label + idx}
-              onClick={() => handleChooseAnchor(href)}
             >
               <a href={`#${href}`}>{label}</a>
             </li>

0fba651bfefebcc6dd1373721c5c8e45a7efc95a
ThyeeZz <jialian.ji@zilliz.com>
2024-06-25 11:49:45 +0800

    update api meta desc, reduce lenggth of meta title, add lang attr

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,13 +2,19 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap"
           rel="stylesheet"
+        />
+        <link
+          rel="alternate"
+          href="/"
+          hrefLang="en"
+          style={{ display: 'none' }}
         />
       </Head>
       <body>

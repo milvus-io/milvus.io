0c589acca0c2901b3f74a63cff5becb9e251c70c
Shuyou <shuyoou@outlook.com>
2024-09-14 11:03:07 +0800

    feat: restful mdx supported

---
+++ src/components/mdx/rest-space/BaseURL.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { i18n } from './i18n';
+import { getBaseUrl, isControlPlane } from './utils';
+import styles from './RestSpace.module.css';
+import { Admonition } from './Admonition';
+import { CodeBlock } from './CodeBlock';
+
+export const BaseURL = ({ endpoint, lang, target }) => {
+  const { server, children, prompt } = getBaseUrl(endpoint, lang, target);
+  return (
+    <>
+      <section>
+        <section className={styles.sectionHeader}>
+          <span>
+            {isControlPlane(endpoint)
+              ? 'Base URL'
+              : i18n[lang]['title.cluster.endpoint']}
+          </span>
+        </section>
+        <div style={{ margin: '1rem 0' }}>
+          <p>{i18n[lang]['base.url.format.prompt']}</p>
+          <p className={styles.paramName} style={{ fontSize: '0.9rem' }}>
+            {server}
+          </p>
+        </div>
+        {prompt && (
+          <Admonition
+            type="info"
+            icon="📘"
+            title={i18n[lang]['admonition.title']}
+          >
+            <div dangerouslySetInnerHTML={{ __html: prompt }} />
+          </Admonition>
+        )}
+      </section>
+      <section className={styles.exampleContainer}>
+        <CodeBlock className="language-shell" children={children} />
+      </section>
+    </>
+  );
+};
---
+++ src/utils/mdx.ts
@@ -0,0 +1,71 @@
+import * as babel from '@babel/core';
+import { compile } from '@mdx-js/mdx';
+import traverse from '@babel/traverse';
+
+function extractValue(node) {
+  if (node.type === 'StringLiteral') {
+    return node.value;
+  } else if (node.type === 'NumericLiteral') {
+    return node.value;
+  } else if (node.type === 'ObjectExpression') {
+    const obj = {};
+    node.properties.forEach(prop => {
+      const key = prop.key.name || prop.key.value;
+      const value = extractValue(prop.value);
+      obj[key] = value;
+    });
+    return obj;
+  } else if (node.type === 'ArrayExpression') {
+    return node.elements.map(el => extractValue(el));
+  } else if (
+    node.type === 'ArrowFunctionExpression' ||
+    node.type === 'FunctionExpression'
+  ) {
+    return 'Function';
+  } else if (node.type === 'TemplateLiteral') {
+    const quasis = node.quasis.map(quasi => quasi.value.raw);
+    const expressions = node.expressions.map(expr => extractValue(expr));
+    let result = '';
+    for (let i = 0; i < quasis.length; i++) {
+      result += quasis[i];
+      if (i < expressions.length) {
+        result += expressions[i];
+      }
+    }
+    return result;
+  } else if (node.type === 'Identifier') {
+    return '${' + node.name + '}';
+  } else {
+    return 'Unknown type';
+  }
+}
+
+export async function getVariablesFromMDX(mdxContent) {
+  // Compile the MDX content into JSX
+  const jsxCode = await compile(mdxContent);
+
+  // Use Babel to parse the JSX into an AST
+  const ast = babel.parse(jsxCode.value, {
+    sourceType: 'module',
+    presets: ['@babel/preset-react'],
+  });
+
+  const exports = {};
+
+  // Traverse the AST and extract exports
+  traverse(ast, {
+    ExportNamedDeclaration(path) {
+      if (path.node.declaration && path.node.declaration.declarations) {
+        path.node.declaration.declarations.forEach(declaration => {
+          const key = declaration.id.name;
+          const value = extractValue(declaration.init); // Use extractValue helper
+          exports[key] = value;
+        });
+      }
+    },
+  });
+
+  return {
+    exports,
+  };
+}

8f2b6002ef8bff203d5fa8a03faec7dce0142327
mimo.oyn <ning.ouyang@zilliz.com>
2024-11-28 15:04:30 +0800

    feat: new design for blog page

--- src/components/card/BlogCard.tsx
+++ src/components/card/BlogCard.tsx
@@ -4,25 +4,37 @@ import styles from './BlogCard.module.less';
 import Tags from '../tags';
 import clsx from 'clsx';
 
-const BlogCard = (props: {
+interface Props {
   title: string;
   desc: string;
   tags: string[];
   cover: string;
   path: string;
   className?: string;
-}) => {
-  const { title, desc, tags, cover, path, className } = props;
+  direction?: 'row' | 'column';
+}
+
+const BlogCard: React.FC<Props> = props => {
+  const {
+    title,
+    desc,
+    tags,
+    cover,
+    path,
+    direction = 'column',
+    className,
+  } = props;
   const to = `/blog/${path}`;
 
   return (
     <Link
       href={to}
       className={clsx(styles.BlogCardWrapper, {
+        [styles.row]: direction === 'row',
         [className]: className,
       })}
     >
-      <div className="w-full aspect-[1.59817/1] overflow-hidden rounded-[12px] mb-[28px]">
+      <div className="w-full aspect-[1.59817/1] overflow-hidden rounded-[12px] mb-[28px] cover-container">
         <img className={styles.cover} src={cover} alt={title} />
       </div>
       <div className={styles.descWrapper}>

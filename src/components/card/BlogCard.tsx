b289edda523c00a2a70832b8edc1e15bbd441f96
mimo.oyn <ning.ouyang@zilliz.com>
2024-12-04 10:48:14 +0800

    fix: update blog keep reading card ui

--- src/components/card/BlogCard.tsx
+++ src/components/card/BlogCard.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import Link from 'next/link';
+import { useRouter, usePathname } from 'next/navigation';
 import styles from './BlogCard.module.less';
 import Tags from '../tags';
 import clsx from 'clsx';
+import Link from 'next/link';
 
 interface Props {
   title: string;
@@ -12,6 +13,7 @@ interface Props {
   path: string;
   className?: string;
   direction?: 'row' | 'column';
+  disableWrapperLink?: boolean;
 }
 
 const BlogCard: React.FC<Props> = props => {
@@ -23,16 +25,26 @@ const BlogCard: React.FC<Props> = props => {
     path,
     direction = 'column',
     className,
+    disableWrapperLink = false,
   } = props;
   const to = `/blog/${path}`;
+  const router = useRouter();
+
+  const handleWrapperClick = () => {
+    if (disableWrapperLink) {
+      return;
+    }
+    router.push(to);
+  };
 
   return (
-    <Link
-      href={to}
+    <div
       className={clsx(styles.BlogCardWrapper, {
         [styles.row]: direction === 'row',
         [className]: className,
+        [styles.wrapperLinkDisabled]: disableWrapperLink,
       })}
+      onClick={handleWrapperClick}
     >
       <div className="w-full aspect-[1.59817/1] overflow-hidden rounded-[12px] mb-[28px] cover-container">
         <img className={styles.cover} src={cover} alt={title} />
@@ -41,9 +53,11 @@ const BlogCard: React.FC<Props> = props => {
         <div className={styles.bottomWrapper}>
           <Tags list={tags} tagsClass={styles.tags} />
         </div>
-        <h6 className={styles.title}>{title}</h6>
+        <h6 className={styles.title}>
+          <Link href={to}>{title}</Link>
+        </h6>
       </div>
-    </Link>
+    </div>
   );
 };
 

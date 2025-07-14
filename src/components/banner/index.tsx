1f072b5484584870d982a0af920b113df8e3cdbd
ThyeeZz <jialian.ji@zilliz.com>
2025-07-14 14:58:18 +0800

    update style of banner
    update style of banner

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -3,6 +3,7 @@ import classes from './index.module.less';
 import { useTranslation, Trans } from 'react-i18next';
 import { useGlobalLocale } from '@/hooks/use-global-locale';
 import useUtmTrackPath from '@/hooks/use-utm-track-path';
+import Link from 'next/link';
 
 export default function CloudBanner() {
   const { locale } = useGlobalLocale();
@@ -10,19 +11,12 @@ export default function CloudBanner() {
   const trackPath = useUtmTrackPath();
 
   return (
-    <div className={classes.bannerContainer}>
-      <p className={classes.bannerText}>
-        <Trans
-          t={t}
-          i18nKey="banner"
-          components={[
-            <a
-              href={`${CLOUD_SIGNUP_LINK}?utm_source=milvusio&utm_medium=referral&utm_campaign=top_banner&utm_content=${trackPath}`}
-              key="zilliz-cloud"
-            ></a>,
-          ]}
-        />
-      </p>
-    </div>
+    <Link
+      className={classes.bannerContainer}
+      href={`${CLOUD_SIGNUP_LINK}?utm_source=milvusio&utm_medium=referral&utm_campaign=top_banner&utm_content=${trackPath}`}
+    >
+      <p className={classes.bannerText}>{t('banner')}</p>
+      <button className={classes.bannerButton}>{t('bannerButton')}</button>
+    </Link>
   );
 }

4bf85224fef8bc169a9fea15723247dd33f3e1d8
ThyeeZz <jialian.ji@zilliz.com>
2025-07-15 11:53:58 +0800

    update tsyle of banner bar

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,22 +1,28 @@
 import { CLOUD_SIGNUP_LINK } from '@/consts';
 import classes from './index.module.less';
-import { useTranslation, Trans } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { useGlobalLocale } from '@/hooks/use-global-locale';
 import useUtmTrackPath from '@/hooks/use-utm-track-path';
-import Link from 'next/link';
 
 export default function CloudBanner() {
   const { locale } = useGlobalLocale();
   const { t } = useTranslation('home', { lng: locale });
   const trackPath = useUtmTrackPath();
 
+  const handleRegister = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
   return (
-    <Link
+    <a
       className={classes.bannerContainer}
       href={`${CLOUD_SIGNUP_LINK}?utm_source=milvusio&utm_medium=referral&utm_campaign=top_banner&utm_content=${trackPath}`}
+      target="_blank"
     >
-      <p className={classes.bannerText}>{t('bannerText')}</p>
-      <button className={classes.bannerButton}>{t('bannerButton')}</button>
-    </Link>
+      <button className={classes.bannerText} role="link">
+        {t('bannerText')}
+      </button>
+      <button className={classes.bannerButton} role="link">
+        {t('bannerButton')}
+      </button>
+    </a>
   );
 }
